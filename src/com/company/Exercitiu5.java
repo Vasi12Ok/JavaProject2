74f2a329e71092fa87357307c58a74e79be62733
Sipos <vasi.sipos78@gmail.com>
2022-03-30 14:22:23 +0200

    Tema Exercitiu2

--- src/com/company/Exercitiu5.java
+++ src/com/company/Exercitiu5.java
@@ -4,15 +4,15 @@ public class Exercitiu5 {
 
     public static void main(String[] args) {
 
-                System.out.println(" +\"\"\"\"\"+");
-                System.out.println("[| o o |]");
-                System.out.println(" |  ^  | ");
-                System.out.println(" | '-' |");
-                System.out.println(" +-----+");
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
 
 
-            }
-        }
+    }
+}
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,21 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Ai facut armata ");
-
-      }
+        System.out.println("Ai facut tema pentru scoala!");
     }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
