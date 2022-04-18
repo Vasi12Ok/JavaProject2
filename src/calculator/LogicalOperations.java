5363c8692454bc17458dba80cea2e70efafa3cd5
Sipos <vasi.sipos78@gmail.com>
2022-04-18 11:02:38 +0200

    Tema for loops si while loops

--- src/calculator/LogicalOperations.java
+++ src/calculator/LogicalOperations.java
@@ -3,7 +3,6 @@ package calculator;
 public class LogicalOperations {
     public static void main(String[] args) {
 
-
 //    public int checkBiggerNumber(int firstNumber, int secondNumber) {
 //
 //       System.out.println();
@@ -258,14 +257,216 @@ public class LogicalOperations {
 //                System.out.println(sum/100f);
 
 
-
 //             9. Creat o metoda in clasa LogicalOp care sa afiseze urmatorul tipar:
-       int term=7;
-        for(int i=1;i<=term;i++){
-                for(int j=term;j>=i;j--){
-                System.out.print("* ");
+//       int term=7;
+//        for(int i=1;i<=term;i++){
+//                for(int j=term;j>=i;j--){
+//                System.out.print("* ");
+//                }
+//                System.out.println();//new line
+
+
+//       While Loop
+//              1. Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru. Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la 100, pornind de la numarul primit ca si parametru.
+//                Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+
+//          declare variables
+//          1. Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru. Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la 100, pornind de la numarul primit ca si parametru.
+//             Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+        public void countToHundredWhile ( int givenNumber){
+
+            while (givenNumber <= 100) {
+
+                System.out.println(givenNumber);
+
+                givenNumber++;
+
+            }
+
+//            2. Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru. Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la -100, pornind de la numarul primit ca si parametru.
+//            Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+
+            public void countBackwardsWhile ( int givenNumber){
+
+                while (givenNumber >= -100) {
+
+                    System.out.println(givenNumber);
+
+                    givenNumber--;
+
                 }
-                System.out.println();//new line
+
+            }
+//            3. Creati o metoda in clasa LogicalOp, care sa primeste doi parametrii de tip numar. Metoda sa afiseze o numaratoare intre cele doua numere primite, pornind de la primul parametru primit, pana la cel de-al doilea(ex: daca int x e primul parametru iar int y  este cel de-al doilea, numaratoarea sa se faca de la x, la y.
+//                    Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+
+            public void countBetweenWhile ( int firstNumber, int secondNumber){
+
+                while (firstNumber <= secondNumber) {
+
+                    System.out.println(firstNumber);
+
+                    firstNumber++;
+
                 }
+
+            }
+//            4. Creati o metoda in clasa LogicalOp, care sa primeste doi parametrii de tip numar. Metoda trebuie sa verifice care dintre cele doua numere este mai mare, si sa execute o numaratoare crescatoare de la cel mai mic la cel mai mare.(ex: daca int x e primul parametru iar int y  este cel de-al doilea, daca x  este mai mare decat y,  atunci numaratoarea sa fie de la y la x).
+//                Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+
+            public void countBetweenWhile ( int firstNumber, int secondNumber){
+
+                if (firstNumber < secondNumber) {
+
+                    while (firstNumber <= secondNumber) {
+
+                        System.out.println(firstNumber);
+
+                        firstNumber++;
+
+                    }
+
+                } else if (secondNumber < firstNumber) {
+
+                    while (secondNumber <= firstNumber) {
+
+                        System.out.println(firstNumber);
+
+                        firstNumber++;
+
+                    }
+
+                } else {
+
+                    System.out.println("The numbers are equal");
+
                 }
+
+            }
+//            5. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele pare de la 1 la 100.
+            public void evenNumbersWhile () {
+
+                int number = 1;
+
+                while (number <= 100) {
+
+                    number++;
+
+                    if (number % 2 == 0) {
+
+                        System.out.println(number);
+
+                    }
+
                 }
+
+            }
+//            6. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele impare de la 1 la 100.
+
+            public void unevenNumbersWhile () {
+
+                int number = 1;
+
+                while (number <= 100) {
+
+                    number++;
+
+                    if (number % 2 != 0) {
+
+                        System.out.println(number);
+
+                    }
+
+                }
+
+            }
+//            7. Creati o metoda care sa adune toate numerele din intervalul 111 si 8899. Sa se afiseze suma totala, iar dupa aceea sa se calculeze si afiseze media numerelor din acel interval. Introduceti o variabila int count in metoda, pentru a numara toate numerele din interval. Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
+
+            public void sumAndAverageWhile () {
+
+                int firstNumber = 111;
+
+                int secondNumber = 8899;
+
+                double sum = 0;
+
+                int count = 0;
+
+
+                while (firstNumber <= secondNumber) {
+
+                    sum += firstNumber;
+
+                    firstNumber++;
+
+                    count++;
+
+                }
+
+                System.out.println(sum);
+
+                double average = sum / count;
+
+                System.out.println("The average of the numbers is: " + average);
+
+            }
+        }
+//        8. Creati o metoda care sa primeasca doi parametrii de tip numar, si sa returneze media numerelor divizibile cu 7, din acel interval. Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
+
+        public float divideBySevenWhile(int firstNumber, int lastNumber) {
+
+            float count = 0F;
+
+            float sum = 0F;
+
+            while (firstNumber <= lastNumber) {
+
+                if (firstNumber % 7 == 0) {
+
+                    count ++;
+
+                    sum += firstNumber;
+
+                }
+
+                firstNumber++;
+
+            }
+
+            float average = sum / count;
+
+            return average;
+
+        }
+//        9. Creati o metoda care sa afiseze primele 20 de numere din sirul lui Fibonacci.
+
+        public void fibonacci() {
+
+            int count = 20;
+
+            int num1 = 0;
+
+            int num2 = 1;
+
+
+
+            int i=1;
+
+            while(i<=count)
+
+            {
+
+                System.out.print(num1+" ");
+
+                int sumOfPrevTwo = num1 + num2;
+
+                num1 = num2;
+
+                num2 = sumOfPrevTwo;
+
+                i++;
+
+            }
+
+        }
+    }
---
+++ src/calculator/Main5.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+//   1. Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru. Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la 100, pornind de la numarul primit ca si parametru.
+//    Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
+
+
+    }
+}
